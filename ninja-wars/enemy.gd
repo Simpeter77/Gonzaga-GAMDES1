c378bca148fbc2fdd057d2a68785a5e0725e8ded
Simpeter77 <20233022@s.ubaguio.edu>
2025-09-06 07:15:39 +0800

    .

--- ninja-wars/enemy.gd
+++ ninja-wars/enemy.gd
@@ -14,29 +14,30 @@ func _ready() -> void:
 	if start_delay > 0:
 		await get_tree().create_timer(start_delay).timeout
 	can_move = true
-	
 
 func _physics_process(delta: float) -> void:
 	if not can_move:
 		return
+
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	# Move enemy left/right
+
+	# Move enemy left to right vv
 	velocity.x = direction * SPEED
 	character_sprite.flip_h = direction < 0
 
 	move_and_slide()
 
+	#  Check collisions after moving
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		if collision.get_collider().is_in_group("player"):
+			_game_over()
+
 	# Reverse direction when hitting a wall
 	if is_on_wall():
 		direction *= -1
-
-	# Check collision with player
-	for i in range(get_slide_collision_count()):
-		var collision = get_slide_collision(i)
-		var collider = collision.get_collider()
-		if collider is CharacterBody2D and collider.is_in_group("Player"):
-			_game_over()
 
 func _game_over() -> void:
 	print("Game Over: Enemy touched the player")
--- ninja-wars/player.gd
+++ ninja-wars/player.gd
@@ -1,41 +1,48 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 const JUMP_VELOCITY = -800.0
 
 @onready var character_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
-	if (velocity.x>1 || velocity.x<-1):
+	if (velocity.x > 1 or velocity.x < -1):
 		character_sprite.animation = "run"
 	else:
 		character_sprite.animation = "default"
-	# Add the gravity.
+
+	# Gravity
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 		character_sprite.animation = "jump"
-	
-	# Handle jump.
+
+	# Jump
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		character_sprite.animation = "jump"
-	
-	if Input.is_action_just_pressed("duck") and !is_on_floor():
+
+	# Duck (mid-air boost upward in your code)
+	if Input.is_action_just_pressed("duck") and not is_on_floor():
 		velocity.y = -1 * JUMP_VELOCITY
 		character_sprite.animation = "duck"
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Movement
 	var direction := Input.get_axis("left", "right")
-	
-	#var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, 45)
 
-	var isLeft = velocity.x <0
-	character_sprite.flip_h = isLeft
-	
+	character_sprite.flip_h = velocity.x < 0
+
 	move_and_slide()
+	
+	#  Check collisions after moving
+	for i in range(get_slide_collision_count()):
+		var collision = get_slide_collision(i)
+		if collision.get_collider().is_in_group("enemya"):
+			_game_over()
+	
+func _game_over() -> void:
+	print("Game Over: Player touched the enemy")
+	get_tree().reload_current_scene()
